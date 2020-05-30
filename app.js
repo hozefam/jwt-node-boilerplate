e7537e19dd7bb6cb087a4eb465de323745febc8a
Hozefa <hozefam@gmail.com>
2020-05-31 01:45:48 +0300

    Initial Checkin

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const app = express();
+const UserModel = require('./model/model');
+
+mongoose.connect('mongodb://127.0.0.1:27017/passport-jwt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('error', (error) => console.log(error));
+mongoose.Promise = global.Promise;
+
+require('./auth/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const routes = require('./routes/routes');
+const secureRoute = require('./routes/secure-routes');
+
+app.use('/', routes);
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+app.listen(3000, () => {
+  console.log('Server started');
+});
